77bb25eac5e2a02f58db70de81f3163239b64009
jteitelb <7592143+jteitelb@users.noreply.github.com>
2024-02-01 12:57:49 -0500

    rendering, random highlighted square every 30 frames

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ who could see this and not want to re-create it?
 [the post that inspired me](https://twitter.com/vnglst/status/1751278052154179770?t=aEY-nyBzjO4Sp0JI5tA7bA)
 
 [the post that inspired the post that inspired me](https://twitter.com/nicolasdnl/status/1749715070928433161)
+
+learn to make games with the Canvas API:
+https://dev.to/zippcodder/complete-guide-to-building-games-in-the-browser-kp6
--- main.js
+++ main.js
@@ -1,14 +1,58 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#eee";
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10; j++) {
-    if ((i + j) % 2 == 0) {
-      ctx.fillStyle = "#000";
-    } else {
-      ctx.fillStyle = "#fff";
+const GLOBALS = {
+  highlightedTile: { active: true, x: 9, y: 0 },
+  nextHighlightFrame: 30,
+};
+
+const PROPS = [];
+
+const CHARS = [];
+
+function init() {}
+
+function renderBackground() {
+  if (GLOBALS.nextHighlightFrame > 0) {
+    GLOBALS.nextHighlightFrame--;
+  } else {
+    GLOBALS.nextHighlightFrame = 30;
+    GLOBALS.highlightedTile.x = Math.floor(Math.random() * 10);
+    GLOBALS.highlightedTile.y = Math.floor(Math.random() * 10);
+  }
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (
+        GLOBALS.highlightedTile.active &&
+        GLOBALS.highlightedTile.x == i &&
+        GLOBALS.highlightedTile.y == j
+      ) {
+        ctx.fillStyle = "#da4";
+      } else if ((i + j) % 2 == 0) {
+        ctx.fillStyle = "#000";
+      } else {
+        ctx.fillStyle = "#fff";
+      }
+      ctx.fillRect(40 * i, 40 * j, 40, 40);
     }
-    ctx.fillRect(40 * i, 40 * j, 40, 40);
   }
 }
+
+function renderProps() {}
+
+function renderCharacters() {}
+
+function renderControls() {}
+
+function startFrames() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  renderBackground();
+  renderProps();
+  renderCharacters();
+  renderControls();
+
+  window.requestAnimationFrame(startFrames);
+}
+
+startFrames();
