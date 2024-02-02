c59d5fbc7459adc58136cc7766983af86cf3867d
jteitelb <7592143+jteitelb@users.noreply.github.com>
2024-02-02 16:39:34 -0500

    show colors and reset button

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const newColors = document.getElementById("newColors");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const color3 = document.getElementById("color3");
+
+const balls = [];
+const colors = [];
+
+function generateBalls() {
+  balls.length = 0;
+  colors.length = 0;
+  for (let i = 0; i < 3; i++) {
+    let red = parseInt(Math.random() * 256);
+    let green = parseInt(Math.random() * 256);
+    let blue = parseInt(Math.random() * 256);
+    colors.push(`rgb(${red}, ${green}, ${blue})`);
+    color1.innerHTML = colors[0];
+    color2.innerHTML = colors[1];
+    color3.innerHTML = colors[2];
+    for (let j = 0; j < 8; j++) {
+      balls.push(
+        new Ball(Math.random() * 330 + 20, Math.random() * 330 + 20, colors[i])
+      );
+    }
+  }
+}
+
+newColors.addEventListener("click", generateBalls);
+
 function renderBackground() {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
@@ -41,23 +70,6 @@ class Ball {
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
-  }
-}
-
-const balls = [];
-
-for (let i = 0; i < 3; i++) {
-  let red = Math.random() * 256;
-  let green = Math.random() * 256;
-  let blue = Math.random() * 256;
-  for (let i = 0; i < 8; i++) {
-    balls.push(
-      new Ball(
-        Math.random() * 330 + 20,
-        Math.random() * 330 + 20,
-        `rgb(${red}, ${green}, ${blue})`
-      )
-    );
   }
 }
 
